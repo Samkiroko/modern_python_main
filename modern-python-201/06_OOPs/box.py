d0bc1cb15cec834b4b358c690599e283ad06d81d
Samkiroko <kirokodev@gmail.com>
2022-11-18 15:44:14 +0300

    Classes

--- modern-python-201/06_OOPs/box.py
+++ modern-python-201/06_OOPs/box.py
@@ -25,7 +25,7 @@ class Box:
     def __eq__(self, __o: object) -> bool:
         """Check if two Boxes are equal"""
         if not isinstance(__o, Box):
-            raise NotImplementedError
+            raise NotImplemented
         return self.side_a == __o.side_a and self.side_b == __o.side_b
 
     def __le__(self, __o: object) -> bool:
@@ -34,8 +34,38 @@ class Box:
             raise NotImplementedError
         return self.area <= __o.area
 
+    def __add__(self, __o: object) -> int:
+        """Adds two Boxes"""
+        if not isinstance(__o, Box):
+            raise NotImplemented
+        return self.area + __o.area
 
-b1 = Box(3, 8)
+    def __sub__(self, __o: object) -> int:
+        """Substracts two Boxes"""
+        if not isinstance(__o, Box):
+            raise NotImplemented
+        return self.area - __o.area
+
+    def __mul__(self, __o: object) -> int:
+        """Multiplication  two Boxes"""
+        if not isinstance(__o, Box):
+            raise NotImplemented
+        return self.area * __o.area
+
+    def __truediv__(self, __o: object) -> float:
+        """division will return a flaot"""
+        if not isinstance(__o, Box):
+            raise NotImplemented
+        return self.area / __o.area
+
+    def __floordiv__(self, __o: object) -> int:
+        """division will return a init"""
+        if not isinstance(__o, Box):
+            raise NotImplemented
+        return self.area // __o.area
+
+
+b1 = Box(8, 8)
 b2 = Box(4, 4)
 
 print(b1)
@@ -47,3 +77,12 @@ print("______equal_end________")
 
 print(4 in b1)
 print(6 in b1)
+
+
+print("______Algebric operation________")
+
+print(b1 - b2)
+print(b1 + b2)
+print(b1 * b2)
+print(b1 / b2)
+print(b1 // b2)
